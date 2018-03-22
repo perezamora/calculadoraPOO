5f13672afc95c0e3ce7cefc7128ea194c94c4e1a
Pere Zamora <pere.zamora@atmira@com>
2018-03-22 15:44:07 +0100

    añadir testeo integrado con Xunit

---
+++ PooVuelingXunitTest/PooVuelingXunitTests.cs
@@ -0,0 +1,35 @@
+﻿using PooVueling;
+using Xunit;
+
+namespace PooVuelingXunitTests
+{
+    public class CalculadoraIntegrationXunitTests
+    {
+        ICalculadora iCalculadora = new Calculadora();
+
+        [Fact]
+        public void SumaTest()
+        {
+            Assert.True(iCalculadora.Suma(7, 3) == 10);
+        }
+
+        [Fact]
+        public void RestaTest()
+        {
+            Assert.True(iCalculadora.Resta(2, 1) == 1);
+        }
+
+        [Fact]
+        public void DivisionTest()
+        {
+            Assert.True(iCalculadora.Division(10, 2) == 5);
+        }
+
+        [Fact]
+        public void MultiplicacionTest()
+        {
+            Assert.True(iCalculadora.Multiplicacion(11, 2) == 22);
+        }
+
+    }
+}
--- README.md
+++ README.md
@@ -1,3 +1,13 @@
-##Testing ejemplo Calculadora simple con framework MSTestv2
+## Testing ejemplo Calculadora simple con framework MSTestv2
 
 Realizamos ejemplo calculadora para POO, realizando testeos.
+
+Con la libreria Testv2 incorporada en el Visual studio.
+
+Con la libreria de terceros Xunit -> instalada a traves visual studio con NuGet:
+	xunit
+	xnuit.runner.console
+	
+comando para ejecutar test desde consola, desde carpet raiz de la solucion.
+
+packages\xunit.runner.console.2.3.1\tools\net452\xunit.console.exe PooVuelingXunitTest\bin\Debug\PooVuelingXunitTest.dll
