667369f94d8b097b4bf85106a9c67314e44091be
Pere Zamora <pere.zamora@atmira@com>
2018-03-25 16:14:54 +0200

    Crear metadatos programaticamente

---
+++ Metadatos/Author.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metadatos
+{
+    // AttributeUsageAttribute:
+    // AttributeTargets: Donde se puede aplicar el atributo
+    // Inherited: Si puede heredarse o no a traves de las clases
+    // AllowMultiple: Si puede exister mas de una instancia del atributo en un elemento
+
+    [System.AttributeUsage(System.AttributeTargets.All)
+    ]
+    public class Author : Attribute
+    {
+        private string name;
+        public double version;
+
+        public Author(string name)
+        {
+            this.name = name;
+            version = 1.0;
+        }
+
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+    }
+}
---
+++ Metadatos/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metadatos
+{
+    [Developer("John Doe", "1")]
+    [Author("Mala Rodriguez")]
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GetAttribute(typeof(Program));
+            Console.ReadKey();
+        }
+
+
+        public static void GetAttribute(Type t)
+        {
+            // Get instance of the attribute.
+            DeveloperAttribute Attrib = (DeveloperAttribute)Attribute.GetCustomAttribute(t, typeof(DeveloperAttribute));
+            Author Attrib1 = (Author)Attribute.GetCustomAttribute(t, typeof(Author));
+
+            if (Attrib == null)
+            {
+                Console.WriteLine("The attribute was not found.");
+            }
+            else
+            {
+                // Get the Name value.
+                Console.WriteLine("The Name Attribute is: {0}.", Attrib.Name);
+                // Get the Level value.
+                Console.WriteLine("The Level Attribute is: {0}.", Attrib.Level);
+                // Get the Reviewed value.
+                Console.WriteLine("The Reviewed Attribute is: {0}.", Attrib.Reviewed);
+            }
+
+            // Metadato Author
+            if (Attrib1 == null)
+            {
+                Console.WriteLine("The attribute 1 was not found.");
+            }
+            else
+            {
+                Console.WriteLine("The Name Author is {0}. ", Attrib1.Name);
+            }
+        }
+    }
+}
